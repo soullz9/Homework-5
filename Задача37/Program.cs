f2f6492213a6360a9a1410cca3c0c5c8be5e7d5b
Maxim <lollollol3328@gmail.com>
2022-12-02 16:04:57 +0300

    Homework-5

---
+++ Задача37/Program.cs
@@ -0,0 +1,16 @@
+﻿int N = 10;
+int[] array = new int[N];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int j = array.Length - 1;
+for (int i = 0; i < (array.Length + 1) / 2; i++)
+{
+    Console.Write(array[i] * array[j] + " ");
+    j--;
+}
